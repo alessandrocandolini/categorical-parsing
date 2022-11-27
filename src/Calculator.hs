e43ed9d21f2ea6c7ae82b9f54bfb5087a63898a9
Alessandro Candolini <alessandrocandolini@gmail.com>
2022-11-27 01:53:09 +0000

    recursion schemes

--- src/Calculator.hs
+++ src/Calculator.hs
@@ -1,17 +1,37 @@
-module Calculator where
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE FlexibleInstances #-}
+module Calculator(Ast, evaluate) where
 
-import Parser 
+import Parser
+import Data.Fix(Fix(..))
+import Data.Functor.Foldable (cata)
 
-data Ast a = 
-   Add (Ast a) (Ast a) | 
-   Multiply (Ast a) (Ast a) | 
-   Lit a 
+data AstF a b =
+   AddF b b |
+   MultiplyF  b b |
+   LitF a deriving (Functor, Foldable)
 
-evaluate :: Num a => Ast a  -> a 
-evaluate (Add v1 v2) = evaluate v1 + evaluate v2 
-evaluate (Multiply v1 v2) = evaluate v1 * evaluate v2 
-evaluate (Lit a) = a 
+type Ast a = Fix(AstF a )
 
-ast :: Parser a -> Parser (Ast a) 
+instance Num a => Num (Ast a) where
+  a1 + a2 = Fix $AddF a1 a2
+  a1 * a2 = Fix $ MultiplyF a1 a2
+  fromInteger = Fix . LitF . fromInteger
+
+evalAlg :: Num a => AstF a a -> a
+evalAlg (LitF a) = a
+evalAlg (AddF a1 a2) = a1 + a2
+evalAlg (MultiplyF a1 a2) = a1 * a2
+
+evaluate :: Num a => Ast a -> a
+evaluate = cata evalAlg
+
+--evaluate :: Num a => Ast a  -> a
+--evaluate (Add v1 v2) = evaluate v1 + evaluate v2
+--evaluate (Multiply v1 v2) = evaluate v1 * evaluate v2
+--evaluate (Lit a) = a
+
+ast :: Parser a -> Parser (Ast a)
 ast = undefined -- add <|> multiply <|> lit
 
--- test/CalculatorSpec.hs
+++ test/CalculatorSpec.hs
@@ -9,7 +9,7 @@ spec :: Spec
 spec = describe "Simple test" $ do
 
      it "evaluate ast" $
-        evaluate (Multiply (Lit 4) (Add (Lit 2) (Lit 3))) `shouldBe` 20
+        evaluate 4 * (3 + 2) `shouldBe` 20
 
      prop "property-based unit test" $
         \l -> reverse ( reverse l ) == ( l::[Int])
